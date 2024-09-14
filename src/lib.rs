8dcec9f17e44fe95292d758339faba4c7ec48d1b
KazukiMiyazato2021 <kazuki.miyazato.2021+git@gmail.com>
2024-09-14 19:02:56 +0900

    ✅ add test files

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use std::io::Error;
 use std::io::Read;
 use std::fs::File;
 
@@ -5,20 +6,12 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
-fn load_file(file_path: &str) -> std::result::Result<std::string::String, std::io::Error> {
-    let f = File::open(file_path);
 
-    let mut f = match f {
-        Ok(file) => file,
-        Err(e) => return Err(e),
-    };
-
+fn load_file(file_path: &str) -> Result<String, Error> {
+    let mut f = File::open(file_path)?;
     let mut s = String::new();
-
-    match f.read_to_string(&mut s) {
-        Ok(_) => Ok(s),
-        Err(e) => Err(e),
-    }
+    let _ = f.read_to_string(&mut s);
+    Ok(s)
 }
 
 #[cfg(test)]
@@ -27,14 +20,9 @@ mod tests {
 
     #[test]
     fn can_read_file_and_convert_to_string() {
-        let path = "sysctl.conf";
-        std::fs::write(path, "endpoint = localhost:3000").expect("Failed to write test file");
         // ファイルを読み込んで内容を確認
-        let result = load_file("sysctl.conf");
+        let result = load_file("tests/case-1.conf");
         assert!(result.is_ok());
-        assert_eq!(result.unwrap(), "endpoint = localhost:3000");
-    
-        // テスト用のファイルを削除
-        std::fs::remove_file("sysctl.conf").expect("Failed to remove test file");
+        assert_eq!(result.unwrap(), "endpoint = localhost:3000\ndebug = true\nlog.file = /var/log/console.log\n");
     }
 }
